3b2e8c8d95101fafe8d31de09db30cbedd6f6f3b
cupcakearmy <nicco.borgioli@gmail.com>
2019-02-16 16:25:26 +0100

    Default DB and session name

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import json
 from src import InstaBot
 
 data = json.load(open('config.json'))
+data['database_name'] = 'data.db'
+data['session_file'] = 'data.session'
 bot = InstaBot(**data)
 
-while True:
-	bot.new_auto_mod()
+bot.mainloop()
